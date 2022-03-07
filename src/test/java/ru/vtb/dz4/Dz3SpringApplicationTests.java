e5cea26fc02e0670fb87c80d2b25b26d456f2ad5
Gena4489 <gn4489@yandex.ru>
2022-03-07 20:05:04 +0300

    first commit

---
+++ src/test/java/ru/vtb/dz4/Dz3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.dz1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
